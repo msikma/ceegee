b7ca57b7cc64117ae270ad74ac80580c9e4c1ffd
Michiel Sikma <michiel@sikma.org>
2016-09-14 11:10:58 +0200

    Clarify what these functions do

--- src/utils/args.c
+++ src/utils/args.c
@@ -11,6 +11,9 @@
 #include "src/utils/args.h"
 #include "src/utils/version.h"
 
+/**
+ * Prints the program's usage information, MS-DOS style.
+ */
 void print_usage(char *name) {
     printf("%s, %s\r\n", get_name(), get_copyright());
     printf("\r\n");
@@ -23,11 +26,16 @@ void print_usage(char *name) {
     printf("More information: %s\r\n", get_url());
 }
 
+/**
+ * Parses command-line arguments and returns one of the ARG_* macros.
+ * MS-DOS style slash arguments are accepted.
+ */
 int parse_args(int argc, char **argv) {
     if (argc <= 1) {
         return ARG_NOTHING;
     }
 
+    // Skip over the program name.
     for (int a = 1; a < argc; ++a) {
         if (strcmp(argv[a], "/?") == 0) {
             return ARG_USAGE;
